19b324f60d92a9f27a500bcadbbf2d1ba8777f80
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-05 13:34:42 +0000

    modified brain-even

--- brain_games/brain_even.py
+++ brain_games/brain_even.py
@@ -1,50 +1,40 @@
 from random import randint
-from cli import welcome_user
-from brain_games import main
-
-from random import randint
 import prompt
 
-DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-
-
-def display_rules():
-    """Display the rules of the game."""
-    print("Welcome to the Brain Even Games!")
-    print(DESCRIPTION)
-
-
-def make_question_and_correct_answer():
-    """Generate a question and its correct answer."""
-    min_number = 1
-    max_number = 99
-    number = randint(min_number, max_number)
-    question = str(number)
-    correct_answer = 'yes' if number % 2 == 0 else 'no'
-    return question, correct_answer
-
-
 def main():
-    display_rules()
+    print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
 
-    for _ in range(3):  # Allow the user three attempts
-        question, correct_answer = make_question_and_correct_answer()
+    def main_num():
+        min_num = 1
+        max_num = 99
+        number = randint(min_num, max_num)
+        question = str(number)
+
+        if number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+
+        print("Answer 'yes' if the number is even, otherwise answer 
+'no'.")
+        return question, correct_answer
+
+    for _ in range(3):  # Позволим пользователю три попытки
+        question, correct_answer = main_num()
         print(f"Question: {question}")
         user_answer = prompt.string("Your answer: ")
 
-        if user_answer.lower() == correct_answer:
-            print("Correct!")
-        else:
+        if user_answer != correct_answer:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was 
 '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
-    print(f"Congratulations, {name}!")
+        print("Correct!")
 
+    print(f"Congratulations, {name}!")
 
 if __name__ == "__main__":
     main()
-
