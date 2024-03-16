3f92e193a3cd746e79ec658a25a3a6c7ad89c058
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-16 16:02:22 +0000

    update calc.py

---
+++ brain_games/scripts/calc.py
@@ -0,0 +1,11 @@
+import brain_games.games.brain_calc
+import brain_games.engine
+
+
+def main():
+    game = brain_games.games.brain_calc
+    brain_games.engine.run_start_to_end(game)
+
+
+if __name__ == '__main__':
+    main()
