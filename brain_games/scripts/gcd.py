7e6c7d5a35e8bfe6d0f825b02e3928611436198a
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-16 16:09:14 +0000

    update the architecture

---
+++ brain_games/scripts/gcd.py
@@ -0,0 +1,11 @@
+import brain_games.games.brain_gcd
+import brain_games.engine
+
+
+def main():
+    game = brain_games.games.brain_gcd
+    brain_games.engine.run_start_to_end(game)
+
+
+if __name__ == '__main__':
+    main()
