727723e8abae15cd7156917d6595919b97300fda
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-16 16:07:17 +0000

    update even.py

---
+++ brain_games/scripts/even.py
@@ -0,0 +1,11 @@
+import brain_games.games.brain_even
+import brain_games.engine
+
+
+def main():
+    game = brain_games.games.brain_even
+    brain_games.engine.run_start_to_end(game)
+
+
+if __name__ == '__main__':
+    main()
