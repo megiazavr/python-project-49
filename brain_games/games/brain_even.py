d7449a8c27fa4c40e5366015831e378a8f0b3ce3
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-21 12:38:30 +0000

    brain_even upd

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -36,5 +36,16 @@ def main():
 
     print(f"Congratulations, {name}!")
 
+def generate_question() -> str:
+    '''Generate random integer number and return it
+    as a string'''
+    return str(randint(1, 100))
+
+
+def get_correct_answer(question: str) -> str:
+    '''Take integer number as a string and return string 'yes'
+    if the number is even, otherwise string 'no' if odd'''
+    return 'no' if int(question) % 2 else 'yes'
+
 if __name__ == "__main__":
     main()
