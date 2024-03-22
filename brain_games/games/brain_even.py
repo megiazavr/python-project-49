566572d8af0aa16a9a629c9fa0493c42ab4c2690
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-22 11:07:19 +0000

    upd brain_even

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,8 +1,7 @@
 from random import randint
 
 
-DESCRIPTION = 'Answer "yes" if the number is even, otherwise 
-answer "no".'
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def generate_question() -> str:
