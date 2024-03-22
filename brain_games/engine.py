991388cd4b3ab151f6c15e585c7c304a52901d59
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-03-22 11:43:44 +0000

    engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,26 +1,26 @@
-import prompt
-
-NUMBER_OF_ROUNDS = 3
+import brain_games.cli
 
 
-def engine(game):
-    print('Welcome to the Brain Games!')
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print(game.DESCRIPTION)
-    count = 0
-    while count < NUMBER_OF_ROUNDS:
-        question, correct_answer = game.generate_question_answer()
-        print(question)
-        user_answer = prompt.string('Your answer: ')
-        if correct_answer == user_answer:
+ANSWERS_TO_COMPLETE = 3
+
+
+def run_game(game_module, cli_module=brain_games.cli) -> None:
+    '''Take the game resources module and run the game'''
+    user = cli_module.welcome_user()
+    print(game_module.DESCRIPTION)
+    answers_left = ANSWERS_TO_COMPLETE  # the required number of correct answers
+    while answers_left > 0:
+        question = game_module.generate_question()
+        print(f'Question: {question}')
+        correct_answer = game_module.get_correct_answer(question)
+        user_answer = cli_module.get_user_answer()
+        if user_answer == correct_answer:
+            answers_left -= 1
             print('Correct!')
         else:
-            print(
-                f"'{user_answer}' is wrong answer ;(."
-                f"Correct answer was '{correct_answer}'."
-            )
-            print(f"Let's try again, {user_name}!")
-            return
-        count += 1
-    print(f'Congratulations, {user_name}!')
+            print(f"'{user_answer}' is wrong answer ;(.", end=' ')
+            print(f"Correct answer was '{correct_answer}'.")
+            cli_module.say_game_over(user)
+            break
+    else:
+        cli_module.say_well_done(user)
