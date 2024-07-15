8a87edf633306758bc69f092d70ef8348dc3ba84
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-15 23:12:43 +0100

    change the engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,30 +1,19 @@
 from types import ModuleType
 import prompt
 
-
 ROUNDS = 3
 
-
-def greet_user_and_get_user_name(game_rule: str) -> str:
+def start_game(game: ModuleType) -> None:
     print('Welcome to the Brain Games!')
     user_name = prompt.string("May I have your name? ")
     print(f'Hello, {user_name}!')
-    print(game_rule)
-    return user_name
-
-
-def show_question_get_user_answer(question: str) -> str:
-    print(f'Question: {question}')
-    answer = prompt.string('Your answer: ')
-    return answer
-
-
-def start_game(game: ModuleType) -> None:
-    game_rule = game.game_rule
-    user_name = greet_user_and_get_user_name(game_rule)
+    print(game.game_rule)
+    
     for _ in range(ROUNDS):
         question, right_answer = game.get_question_and_answer()
-        user_answer = show_question_get_user_answer(question)
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
+        
         if user_answer == str(right_answer):
             print('Correct!')
         else:
@@ -34,3 +23,4 @@ def start_game(game: ModuleType) -> None:
             break
     else:
         print(f'Congratulations, {user_name}!')
+
