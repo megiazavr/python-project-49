eb17a5e6fd2176269c73458f32d707f6c1d59357
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-17 22:45:23 +0100

    change brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -2,22 +2,23 @@ from random import randint
 
 game_rule = 'What number is missing in the progression?'
 
-
 def get_question_and_answer() -> tuple[str, str]:
-    progression, length = get_progression()
-
-    hide_char_index = randint(0, length - 1)
-    right_answer = str(progression[hide_char_index])
-    progression[hide_char_index] = '..'
-
-    question = " ".join(map(str, progression))
+    progression = generate_progression()
+    question, right_answer = hide_element_in_progression(progression)
     return question, right_answer
 
-
-def get_progression() -> tuple[list, int]:
+def generate_progression() -> list[int]:
     length = randint(5, 11)
     start = randint(1, 20)
     step = randint(2, 6)
     stop = start + (length * step)
     progression = list(range(start, stop, step))
-    return progression, length
+    return progression
+
+def hide_element_in_progression(progression: list[int]) -> tuple[str, str]:
+    hide_char_index = randint(0, len(progression) - 1)
+    right_answer = str(progression[hide_char_index])
+    progression[hide_char_index] = '..'
+    question = " ".join(map(str, progression))
+    return question, right_answer
+
