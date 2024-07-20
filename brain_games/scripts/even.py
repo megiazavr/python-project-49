18e6c488158a028503c359ec6f61daa8b794c022
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-20 17:09:45 +0100

    fix yje build prob

--- brain_games/scripts/even.py
+++ brain_games/scripts/even.py
@@ -1,9 +1,9 @@
 from brain_games.games import brain_even
-from brain_games.engine import start_game
+from brain_games.engine import play_game
 
 
 def main():
-    return start_game(brain_even)
+    return play_game(brain_even)
 
 
 if __name__ == '__main__':
