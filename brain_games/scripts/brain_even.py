56aa0982716f7d2aab4da97960eb3433daf3336c
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-25 23:17:56 +0100

    hh

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,9 @@
-from brain_games.games.even import even
+from brain_games.games.even import get_question_and_answer, game_rule
 from brain_games.engine import play_game
 
 
 def main():
-    return play_game(even)
+    return play_game(get_question_and_answer, game_rule)
 
 
 if __name__ == '__main__':
