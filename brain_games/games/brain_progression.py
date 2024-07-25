97c89a5979757795d2ba0e738061131afbc5b37f
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-25 13:22:16 +0100

    change randint

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,7 +1,5 @@
 from random import randint
 
-game_rule = 'What number is missing in the progression?'
-
 from random import randint
 
 GAME_RULE = 'What number is missing in the progression?'
@@ -13,20 +11,21 @@ MIN_STEP = 2
 MAX_STEP = 6
 
 def get_question_and_answer() -> tuple[str, str]:
-    progression = generate_progression()
-    question, right_answer = hide_element_in_progression(progression)
+    length = randint(MIN_PROGRESSION_LENGTH, MAX_PROGRESSION_LENGTH)
+    start = randint(MIN_START, MAX_START)
+    step = randint(MIN_STEP, MAX_STEP)
+    progression = generate_progression(start, step, length)
+    hide_char_index = randint(0, len(progression) - 1)
+    question, right_answer = hide_element_in_progression(progression, 
+hide_char_index)
     return question, right_answer
-
-def generate_progression() -> list[int]:
-    length = randint(5, 11)
-    start = randint(1, 20)
-    step = randint(2, 6)
+ 
+generate_progression(start: int, step: int, length: int) -> list[int]:
     stop = start + (length * step)
     progression = list(range(start, stop, step))
     return progression
 
 def hide_element_in_progression(progression: list[int]) -> tuple[str, str]:
-    hide_char_index = randint(0, len(progression) - 1)
     right_answer = str(progression[hide_char_index])
     progression[hide_char_index] = '..'
     question = " ".join(map(str, progression))
