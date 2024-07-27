f3e2cc4c67f8cc013d12f88fe94dac2935e888b6
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-27 17:38:35 +0100

    the last one

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,11 +1,13 @@
 import prompt
 from brain_games.constants import ROUNDS_COUNT
 
+
 def play(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.DESCRIPTION)
+
     for _ in range(ROUNDS_COUNT):
         question, right = game.generate_round_data()
         print(f'Question: {question}')
