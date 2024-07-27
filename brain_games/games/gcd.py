05d4715d90d0ee9351266690fceb67ef0ce63a13
Milena Egiaze <milena.egiazaryan77@mail.ru>
2024-07-27 16:06:16 +0100

    sfgbxsg

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,8 +4,8 @@ import math
 DESCRIPTION  = 'Find the greatest common divisor of given numbers.'
 
 def generate_round_data():
-    value1 = random.randint(1, 10)
-    value2 = random.randint(1, 10)
+    value1 = random.randint(1, 100)
+    value2 = random.randint(1, 100)
     question = f'Question: {value1} {value2}'
     correct_answer = str(math.gcd(value1, value2))
     return question, right
